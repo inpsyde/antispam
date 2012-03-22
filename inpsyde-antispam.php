7d390485455210e5310d4c8586ed07832380a84b
Frank Bültge <frank@bueltge.de>
2012-03-22 13:03:42 +0100

    update, new settigns fields and small changes

--- inpsyde-antispam.php
+++ inpsyde-antispam.php
@@ -1,12 +1,15 @@
 <?php 
-/*
-Plugin Name: Inpsyde Antispam
-Plugin URI: 
-Description: Simple Antispam honeypot solution.
-Author: Inpsyde GmbH
-License: GPL
-Version: 1.0
-*/
+/**
+ * Plugin Name: Inpsyde Antispam
+ * Plugin URI:  
+ * Description: Simple Antispam honeypot solution.
+ * Author:      Inpsyde GmbH
+ * Author URI:  http://inpsyde.com
+ * License:     GPLv3
+ * Version:     1.0.0
+ * Text Domain: inps-antispam
+ * Domain Path: /languages
+ */
 
 namespace Inpsyde\Antispam;
 
@@ -33,10 +36,10 @@ add_action( 'comment_form', '\Inpsyde\Antispam\enhance_comment_form' );
  * @return array
  */
 function get_possible_words() {
-
+	
 	$words_string = trim( get_option( 'inpsas_words', '' ) );
-	$raw_words    = explode( "\n", $words_string );
-
+	$raw_words    = explode( "\r\n", $words_string );
+	
 	// filter empty strings
 	return array_filter( $raw_words, function ( $w ) { return strlen( trim( $w ) ) > 0; } );
 }
@@ -68,18 +71,24 @@ function enhance_comment_form( $form ) {
 	
 	// generate/get expected answer
 	$answer = get_random_word();
-
+	
 	// split answer
 	$parts = array();
 	$answer_len = strlen( $answer );
 	$answer_splitpoint = rand( 1, $answer_len - 2 ); // -2 => part 1 cannot be empty
 	$parts[0] = substr( $answer, 0, $answer_splitpoint );
 	$parts[1] = substr( $answer, $answer_splitpoint );
-
+	// custom strings from settings
+	$advice_string = get_option( 'inpsas_advice', '' );
+	if ( empty( $advice_string ) )
+		$advice_string = __( 'Please type the following phrase to confirm you are a human: %word%', 'inps-antispam' );
+	// replace words in string
+	$advice_string = str_replace( '%word%', "%s%s%s", $advice_string );
+	
 	$advice = sprintf(
-	 	__( 'Please type the following phrase to confirm you are a human: "%s%s%s"', 'inps-antispam' ),
+	 	$advice_string,
 	 	$parts[0],
-	 	'<span style="display:none">+</span>',
+	 	'<span style="display:none;">+</span>',
 	 	$parts[1]
 	);
 
@@ -90,11 +99,11 @@ function enhance_comment_form( $form ) {
 		<input type="hidden" name="expected_answer[0]" id="expected_answer_0" value="<?php echo $parts[ 0 ]; ?>">
 		<input type="hidden" name="expected_answer[1]" id="expected_answer_1" value="<?php echo $parts[ 1 ]; ?>">
 		<script type="text/javascript">
-		jQuery(function($) {
-			var answer = $("#expected_answer_0").val() + $("#expected_answer_1").val();
-			$("#inpsyde_antispam_answer").val(answer);
-			$(".hide-if-js-enabled").hide();
-		});
+		jQuery( function($) {
+			var answer = $( "#expected_answer_0" ).val() + $( "#expected_answer_1" ).val();
+			$( "#inpsyde_antispam_answer" ).val(answer);
+			$( ".hide-if-js-enabled" ).hide();
+		} );
 		</script>
 	</div>
 	<?php
@@ -113,7 +122,10 @@ function comment_post( $comment_id ) {
 
 	if ( ! is_current_comment_valid() ) {
 		delete_comment( $comment_id );
-		wp_die( __( 'Sorry, we think you are not a human :/', 'inps-antispam' ) );
+		$rejected_string = get_option( 'inpsas_rejected', '' );
+		if ( empty( $rejected_string ) )
+			$rejected_string = __( 'Sorry, we think you are not a human :/', 'inps-antispam' );
+		wp_die( $rejected_string );
 	}
 }
 
@@ -169,7 +181,11 @@ function recount_comments_for_post( $post_id ) {
 		UPDATE
 			{$wpdb->posts}
 		SET
-			comment_count = (SELECT COUNT(*) from {$wpdb->comments} WHERE comment_post_id = {$post_id} AND comment_approved = '1')
+			comment_count = (SELECT COUNT(*) from {$wpdb->comments} 
+		WHERE
+			comment_post_id = {$post_id}
+		AND
+			comment_approved = '1')
 		WHERE
 			ID = {$post_id}"
 	);
--- settings.php
+++ settings.php
@@ -2,16 +2,17 @@
 namespace Inpsyde\Antispam\Settings;
 
 class Page {
-
+	
 	private $page_hook;
-
+	
 	public function __construct() {
+		
 		add_action( 'admin_menu', array( $this, 'init_menu' ) );
 		add_action( 'admin_init', array( $this, 'init_settings' ) );
 	}
-
 	
 	public function init_menu() {
+		
 		$this->page_hook = add_submenu_page(
 			/* $parent_slug*/ 'options-general.php',
 			/* $page_title */ 'Antispam',
@@ -21,49 +22,81 @@ class Page {
 			/* $function   */ array( $this, 'page' )
 		);
 	}
-
+	
 	public function init_settings() {
+		
 		add_settings_section(
 			/* $id 		 */ 'antispam_settings_section',
 			/* $title 	 */ __( 'Antispam Settings', 'inps-antispam' ),	
 			/* $callback */ function () { /* section head html */ }, 		
 			/* $page	 */ $this->page_hook	
 		);
-
+		
 		add_settings_field(
 			/* $id       */ 'inpsas_words',
 			/* $title    */ sprintf(
 				'<label for="inpsas_words">%s</label>',
 				__( 'Selection of words users without JavaScript have to type. Separate with line breaks.', 'inps-antispam' )
-			),   
+			),
 			/* $callback */ function () {
 				?>
-				<textarea name="inpsas_words" id="inpsas_words" class="large-text code" rows="10" cols="50"><?php echo get_option( 'inpsas_words', '' ); ?></textarea>
+				<textarea name="inpsas_words" id="inpsas_words" rows="10" cols="50"><?php echo get_option( 'inpsas_words', '' ); ?></textarea>
 				<?php
-			},      
+			},
 			/* $page     */ $this->page_hook,  
 			/* $section  */ 'antispam_settings_section'
 		);
-
+		
+		add_settings_field(
+			/* $id       */ 'inpsas_advice',
+			/* $title    */ sprintf(
+				'<label for="inpsas_advice">%s</label>',
+				__( 'Warning, assign the password hint %word% to see it displayed when prompted.', 'inps-antispam' )
+			),
+			/* $callback */ function () {
+				?>
+				<textarea name="inpsas_advice" id="inpsas_advice" class="large-text" rows="10" cols="50"><?php echo get_option( 'inpsas_advice', '' ); ?></textarea>
+				<?php
+			},
+			/* $page     */ $this->page_hook,  
+			/* $section  */ 'antispam_settings_section'
+		);
+		
+		add_settings_field(
+			/* $id       */ 'inpsas_rejected',
+			/* $title    */ sprintf(
+				'<label for="inpsas_rejected">%s</label>',
+				__( 'The warning you wish to give when the field is not or wrong filled out.', 'inps-antispam' )
+			),
+			/* $callback */ function () {
+				?>
+				<textarea name="inpsas_rejected" id="inpsas_rejected" class="large-text" rows="10" cols="50"><?php echo get_option( 'inpsas_rejected', '' ); ?></textarea>
+				<?php
+			},
+			/* $page     */ $this->page_hook,  
+			/* $section  */ 'antispam_settings_section'
+		);
+		
 		register_setting( $this->page_hook, 'inpsas_words' );
+		register_setting( $this->page_hook, 'inpsas_advice' );
+		register_setting( $this->page_hook, 'inpsas_rejected' );
 	}
-
+	
 	public function page() {
 		?>
 		<div class="wrap">
 			<?php screen_icon( 'options-general' ); ?>
-			<h2><?php echo __( 'Antispam', 'inps-antispam' ); ?></h2>
-
+			<h2><?php _e( 'Antispam', 'inps-antispam' ); ?></h2>
+			
 			<form method="post" action="options.php">
 				<?php settings_fields( $this->page_hook ); ?>
 				<?php do_settings_sections( $this->page_hook ); ?>
-
-				<p class="submit">
-					<input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>" />
-				</p>
+				
+				<?php submit_button( __( 'Save Changes' ), 'button-primary', 'submit', TRUE ); ?>
 			</form>
-
+			
 		</div>
 		<?php
 	}
+	
 }
